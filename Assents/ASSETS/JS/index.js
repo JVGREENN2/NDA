6639e8721c5502f34ddd27943738977502774592
JVGREENN2 <annbnn443@gmail.com>
2024-08-07 19:46:02 -0300

    Update index.js

--- Assents/ASSETS/JS/index.js
+++ Assents/ASSETS/JS/index.js
@@ -1,10 +1,22 @@
-const tabs = document.querySelectorAll('.TAB-BTN');
-tabs.forEach(tab => tab.addEventListener('click', () => tabClicked(tab)));
+document.addEventListener('DOMContentLoaded', () => {
+    const tabs = document.querySelectorAll('.TAB-BTN');
 
-const tabClicked = (tab) => {
-  tabs.forEach(tab => tab.classList.remove('active')); tab.classList.add('active');
-  const contents = document.querySelectorAll('.CONTENT'); contents.forEach(content => content.classList.remove('SHOW'));
-  const contentId = tab.getAttribute('CONTENT-id');
-  const content = document.getElementById(contentId); content.classList.add('SHOW');}
+    const tabClicked = (tab) => {
+        tabs.forEach(tab => tab.classList.remove('active'));
+        tab.classList.add('active');
+        
+        const contents = document.querySelectorAll('.CONTENT');
+        contents.forEach(content => content.classList.remove('SHOW'));
+        
+        const contentId = tab.getAttribute('content-id');
+        const content = document.getElementById(contentId);
+        content.classList.add('SHOW');
+    };
 
-  const currentActiveTab = document.querySelector('.TAB-BTN.active'); tabClicked(currentActiveTab);
+    tabs.forEach(tab => tab.addEventListener('click', () => tabClicked(tab)));
+    
+    const currentActiveTab = document.querySelector('.TAB-BTN.active');
+    if (currentActiveTab) {
+        tabClicked(currentActiveTab);
+    }
+});
